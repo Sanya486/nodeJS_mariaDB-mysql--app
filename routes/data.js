c11d2df1b095b4e87370266aca79297d0274599d
Sanya486 <sanyok486@gmail.com>
2023-11-16 15:48:04 +0200

    add error handling

--- routes/data.js
+++ routes/data.js
@@ -2,56 +2,60 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../mariadb/pool');
 
-router.get('/getProducts', async (req, res) => {
+router.get('/getProducts', async (req, res, next) => {
   let conn;
   try {
     conn = await pool.getConnection();
-    const rows = await conn.query('SELECT * from practice.products');
+    const rows = await conn.query('SELECT * from practice.products;');
     console.log(rows);
     res.json(rows).status(200);
   } catch (error) {
-    console.log(error);
+    next(error);
   }
 });
 
-router.post('/addProduct', async (req, res) => {
+router.post('/addProduct', async (req, res, next) => {
   let conn;
   const { name, price } = req.body;
   try {
     conn = await pool.getConnection();
-    await conn.query(
-      `INSERT INTO practice.products(name, price) VALUES('${name}', ${price});`
+    await conn.query(`call practice.InsertProduct('${name}', ${price});`);
+    const result = await conn.query(
+      `SELECT *, "added" as status FROM practice.products where name = "${name}" order by id desc limit 1;`
     );
-    const result = await conn.query('SELECT * FROM practice.products');
     res.json(result).status(200);
   } catch (error) {
-    console.log(error);
+    next(error);
   }
 });
 
-router.delete('/deleteProduct', async (req, res) => {
+router.delete('/deleteProduct', async (req, res, next) => {
   let conn;
   const { name } = req.body;
   try {
     conn = await pool.getConnection();
     await conn.query(`DELETE FROM practice.products where name = "${name}";`);
-    const result = await conn.query('SELECT * FROM practice.products');
+    const result = await conn.query(`SELECT "${name} was deleted" as status;`);
     res.json(result).status(200);
   } catch (error) {
-    console.log(error);
+    next(error);
   }
 });
 
-router.patch('/updateProduct', async (req, res) => {
+router.patch('/updateProduct', async (req, res, next) => {
   let conn;
   const { newName, oldName } = req.body;
   try {
     conn = await pool.getConnection();
-    await conn.query(`UPDATE practice.products SET name = "${newName}" where name = "${oldName}"`);
-    const result = await conn.query('SELECT * FROM practice.products');
+    await conn.query(
+      `UPDATE practice.products SET name = "${newName}" where name = "${oldName}";`
+    );
+    const result = await conn.query(
+      `SELECT * FROM practice.products where name = "${newName}";`
+    );
     res.json(result).status(200);
   } catch (error) {
-    console.log(error);
+    next(error);
   }
 });
 
